14e514ae40c791698e283643e7818ccd5ddee617
vetschn <nicolas.vetsch@gmx.ch>
2021-10-27 15:42:50 +0200

    Added ole_to_datetime

--- src/yadg/dgutils/__init__.py
+++ src/yadg/dgutils/__init__.py
@@ -1,4 +1,4 @@
 #from .dgutils import *
 from .helpers import get_yadg_metadata
-from .dateutils import now, infer_timestamp_from
+from .dateutils import now, infer_timestamp_from, ole_to_datetime
 from .calib import calib_handler

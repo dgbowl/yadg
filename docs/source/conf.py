64f75b91012e9a4c0874ffa952bda8f34037e92b
krpe <peter.kraus@empa.ch>
2021-11-12 10:43:03 +0100

    gitlab ci changes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,8 @@ extensions = [
     #'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     #'sphinx.ext.autosummary',
-    'sphinx_autodoc_typehints'
+    'sphinx_autodoc_typehints',
+    'sphinx_rtd_theme'
     
 ]
 
@@ -51,11 +52,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     "body_max_width": 'none',
-    "stickysidebar": True
+    "sticky_navigation": True,
 }
+html_logo = "./images/yadg.svg" 
+html_favicon = "./images/yadg_ico.png" 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

00236b22f659684a454e6c11abb015bac8c97ad5
Peter Kraus <peter.kraus@geeksonbikes.net>
2022-02-28 15:30:51 +0100

    old versions fix with js

---
+++ versions.js
@@ -0,0 +1,10 @@
+arr = window.location.href.split('/');
+page = arr[arr.length - 1];
+document.write('\
+<dl>\
+    <dt>Versions</dt> \
+    <dd><a href="../master/' + page + '">master</a></dd>\
+    <dd><a href="../4.0.1/' + page + '">4.0.1</a></dd>\
+    <dd><a href="../4.0.0/' + page + '">4.0.0</a></dd>\
+</dl>\
+');

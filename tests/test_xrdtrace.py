5858474fe6577e8b2d36c421fa8725d29a03ef20
krpe <peter.kraus@empa.ch>
2022-04-26 15:06:26 +0200

    Add test for angle and intensity.

--- tests/test_xrdtrace.py
+++ tests/test_xrdtrace.py
@@ -14,63 +14,24 @@ from .utils import datagram_from_input, standard_datagram_test, compare_result_d
     [
         (  # ts0 - xy data
             {"case": "210520step1_30min.xy", "parameters": {"tracetype": "panalytical.xy"}},
-            {"nsteps": 1, "step": 0, "nrows": 1},
+            {"nsteps": 1, "step": 0, "nrows": 1, "point": 0},
         ),
         (  # ts1 - csv data
             {"case": "210520step1_30min.csv", "parameters": {"tracetype": "panalytical.csv"}},
-            {"nsteps": 1, "step": 0, "nrows": 1},
+            {"nsteps": 1, "step": 0, "nrows": 1, "point": 0},
         ),
         (  # ts2 - xml data 
             {"case": "210520step1_30min.xrdml", "parameters": {"tracetype": "panalytical.xrdml"}},
-            {"nsteps": 1, "step": 0, "nrows": 1},
+            {"nsteps": 1, "step": 0, "nrows": 1, "point": 0},
         ),
     ],
 )
 def test_datagram_from_xrdtrace(input, ts, datadir):
-    ret = datagram_from_input(input, "xrdtrace", datadir)
-    print(ret["steps"][0]["data"][0].keys())
-    standard_datagram_test(ret, ts)
-
-
-@pytest.mark.parametrize(
-    "input, refpath",
-    [
-        (
-            {  # ts0 - test.sac
-                "case": "test.sac",
-                "parameters": {"tracetype": "quadstar.sac"},
-            },
-            "xrd_data.json",
-        ),
-        (
-            {  # ts0 - test.sac
-                "case": "test.sac",
-                "parameters": {"tracetype": "quadstar.sac"},
-            },
-            "xrd_data.json",
-        ),
-        (
-            {  # ts0 - test.sac
-                "case": "test.sac",
-                "parameters": {"tracetype": "quadstar.sac"},
-            },
-            "xrd_data.json",
-        ),
-    ],
-)
-
-
-# TODO
-
-
-def test_compare_raw_values(input, refpath, datadir):
-    assert True
-#    os.chdir(datadir)
-#    with open(refpath, "r") as infile:
-#        ref = json.load(infile)["traces"]
-#    ret = datagram_from_input(input, "masstrace", datadir)
-#    ret = ret["steps"][0]["data"][0]["raw"]["traces"]
-#    for key in ret.keys():
-#        for ax in ["m/z", "y"]:
-#            for i in ["n", "s"]:
-#                assert np.allclose(ref[key][ax][i], ret[key][ax][i], equal_nan=True)
+    dg = datagram_from_input(input, "xrdtrace", datadir)
+    standard_datagram_test(dg, ts)
+    with open("xrd_data.json", "r") as inf:
+        ref = json.load(inf)
+    for k in {"intensity", "angle"}:
+        ret = dg["steps"][ts["step"]]["data"][ts["point"]]["raw"]["traces"]["0"][k]
+        compare_result_dicts(ret, ref[k])
+    

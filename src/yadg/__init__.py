f95d93da51cbb0147d61806346b7ce5a112a95cd
krpe <peter.kraus@empa.ch>
2022-04-27 09:22:14 +0200

    black

--- src/yadg/__init__.py
+++ src/yadg/__init__.py
@@ -4,4 +4,5 @@ from .main import run_with_arguments
 sys.path += sys.modules["yadg"].__path__
 
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]

4349df7c926e54a91f8f3b4b9cf6a1a6ffd15931
krpe <peter.kraus@empa.ch>
2022-05-11 07:56:57 +0200

    dgbowl-schemas to 104

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ setuptools.setup(
         "striprtf",
         "tzlocal",
         "python-dateutil",
-        "dgbowl-schemas @ git+https://github.com/PeterKraus/dgbowl-schemas.git@tomatojson#egg=dgbowl_schemas"
+        "dgbowl-schemas>=104"
+        #"dgbowl-schemas @ git+https://github.com/PeterKraus/dgbowl-schemas.git@tomatojson#egg=dgbowl_schemas"
     ],
     extras_require={
         "testing": [

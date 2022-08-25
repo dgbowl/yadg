b088c8bcd1ee3d5cf1483210e8137838bbf90ab0
Peter Kraus <peter.kraus@empa.ch>
2022-08-25 17:02:00 +0800

    Warn if current version is not the latest on PyPI. (#84)
    * add version warning
    
    * Fix >= -> >
    
    * requests
    
    * black

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setuptools.setup(
         "packaging",
         "python-dateutil",
         "openpyxl>=3.0.0",
+        "requests",
         "dgbowl-schemas>=111",
         # Master branch dgbowl-schemas can be installed by:
         # "dgbowl-schemas @ https://github.com/dgbowl/dgbowl-schemas/tarball/master",

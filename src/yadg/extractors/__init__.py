1256bba17e17497f15f8052d23f7fafe13e363a4
Peter Kraus <peter.kraus@tu-berlin.de>
2023-02-22 12:22:49 +0100

    Documentation.

--- src/yadg/extractors/__init__.py
+++ src/yadg/extractors/__init__.py
@@ -24,6 +24,10 @@ for modname in {"eclabmpr", "eclabmpt"}:
 
 
 def load_json(path: Path) -> dict:
+    """
+    Utility function for loading outputs of :func:`extract` stored as ``json`` files
+    into ``dicts`` containing data (nominal and sigma) in :class:`pd.DataFrame` objects.
+    """
     with path.open("r") as inf:
         js = json.load(inf)
     orient = js["yadg_metadata"]["with_orient"]
@@ -37,6 +41,47 @@ def load_json(path: Path) -> dict:
 def extract(
     filetype: str, path: Path, as_dict: bool = True, orient: str = "tight"
 ) -> dict:
+    """
+    Extract worker function.
+
+    Extracts data from provided ``path``, assuming it is the specified ``filetype``. The
+    data is either returned as a ``dict``, where the data tables are dumped by :mod:`pandas`
+    using the appropriate ``orient`` keyword, or they are returned with the data tables
+    intact as :class:`pd.DataFrames`.
+
+    .. warning::
+
+        This feature has been introduced in ``yadg-5.0a2`` and is unstable.
+
+    Parameters
+    ----------
+
+    filetype:
+        Specifies the filetype. Can be both ``yadg``-native as well as a MaRDA extractors
+        :class:`FileType`. Automatically checks for matches using the ``marda:`` prefix.
+
+    path:
+        A :class:`pathlib.Path` object pointing to the file to be extracted.
+
+    as_dict:
+        A :class:`bool` flag (default is ``True``) for toggling ``dict``-like export of
+        extracted data (which can be written using :mod:`json`) instead of returning
+        :class:`pd.DataFrames`.
+
+    orient:
+        A :class:`str` argument with which the :class:`pd.DataFrames` will be exported
+        into :class:`dicts`.
+
+    Returns
+    -------
+
+    ret: dict
+        A dictionary containing the ``yadg_metadata`` and extracted ``content``. Within
+        the content, the ``metadata`` contain any metadata present in the file, the
+        ``values`` and ``sigmas`` contain the nominal values and sigmas of the extracted
+        data, and ``units`` contain the units of columns of dimensional quantities.
+
+    """
 
     for k in {filetype, f"marda:{filetype}"}:
         try:
@@ -52,7 +97,6 @@ def extract(
         ret = {
             "yadg_metadata": {
                 "version": importmeta.version("yadg"),
-                "with_orient": orient,
                 "filename": str(path),
                 "filetype": ftype.filetype,
             },
@@ -63,6 +107,7 @@ def extract(
         }
 
         if as_dict:
+            ret["yadg_metadata"]["with_orient"] = orient
             ret["content"]["values"] = nominal.to_dict(orient=orient)
             ret["content"]["sigmas"] = sigma.to_dict(orient=orient)
         else:

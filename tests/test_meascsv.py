1359124bafd19fb8bf83e2b98c9403dae3f34b65
Peter Kraus <peter.kraus@tu-berlin.de>
2023-03-21 10:54:20 +0100

    Fix meascsv test.

--- tests/test_meascsv.py
+++ tests/test_meascsv.py
@@ -4,6 +4,32 @@ from tests.utils import (
     compare_result_dicts,
 )
 
+from uncertainties import ufloat
+import pint
+import datatree
+from typing import Union
+import numpy as np
+
+def dg_get_quantity(
+    dt: datatree.DataTree,
+    step: Union[str, int],
+    col: str,
+    row: int,
+) -> pint.Quantity:
+    if isinstance(step, str):
+        step = dt[step]
+    else:
+        name = list(dt.children.keys())[step]
+        step = dt[name]
+    
+    n = step[col][row]
+    u = step[col].attrs.get("units", None)
+    if step["_devs"][col].size > 1:
+        d = step["_devs"][col][row]
+    else:
+        d = step["_devs"][col]
+    return pint.Quantity(ufloat(n, d), u)
+    
 
 def test_datagram_from_meascsv(datadir):
     input = {
@@ -13,11 +39,13 @@ def test_datagram_from_meascsv(datadir):
     ret = datagram_from_input(input, "meascsv", datadir)
     standard_datagram_test(ret, {"nsteps": 1, "step": 0, "nrows": 1662, "point": 0})
 
-    compare_result_dicts(
-        ret["steps"][0]["data"][0]["raw"]["T_f"],
-        {"n": 20.9, "s": 0.1, "u": "degC"},
-    )
-    compare_result_dicts(
-        ret["steps"][0]["data"][100]["raw"]["T_f"],
-        {"n": 455.1, "s": 0.1, "u": "degC"},
-    )
+    val = dg_get_quantity(ret, step="0", col="T_f", row=0)
+    assert val.m.n == 20.9
+    assert val.m.s == 0.1
+    assert val.u == pint.Unit("degC")
+    
+    val = dg_get_quantity(ret, step=0, col="T_f", row=100)
+    assert val.m.n == 455.1
+    assert val.m.s == 0.1
+    assert val.u == pint.Unit("degC")
+

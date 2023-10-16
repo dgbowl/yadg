fdfc9cd9903ade3bec4ae3a64619694e39d505f0
Peter Kraus <peter.kraus@tu-berlin.de>
2023-10-16 15:55:51 +0200

    5.0 docs

--- versions.js
+++ versions.js
@@ -4,6 +4,7 @@ document.write('\
 <dl>\
     <dt>Versions</dt> \
     <dd><a href="../master/' + page + '">master</a></dd>\
+    <dd><a href="../5.0/' + page + '">5.0</a></dd>\
     <dd><a href="../4.2/' + page + '">4.2</a></dd>\
     <dd><a href="../4.1/' + page + '">4.1</a></dd>\
     <dd><a href="../4.0.1/' + page + '">4.0.1</a></dd>\

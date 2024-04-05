17cf801dc187810de33be40eff08e65f08596805
Peter Kraus <peter.kraus@tu-berlin.de>
2024-04-05 10:41:40 +0200

    Further tests & fixes

--- src/yadg/extractors/touchstone/snp.py
+++ src/yadg/extractors/touchstone/snp.py
@@ -29,11 +29,39 @@ Schema
 
 Metadata
 ````````
+Metadata about the device are inferred from the structure of the comments lines, see
+below. In most circumstances, the following information can be provided:
+
+- ``Ref R``: the reference resistance (in Ohms)
+- ``Model``: model number of the network analyseer
+- ``Serial``: serial number of the device
+- ``Software Version``: software version with which the file was created
+
+.. note::
+
+    The timestamp (``uts``) is also parsed from the comments in the file. In case you
+    have Touchstone files with a well-defined header that is not supported by yadg,
+    please open an issue.
 
 Uncertainties
 `````````````
 Uncertainties in all variables are determined from the precision of the string-to-float
 conversion.
+
+Notes on file structure
+```````````````````````
+The Touchstone ``.sNp`` files are composed of four sections:
+
+- options line, denoted by ``#``
+- any number of comments lines, denoted by ``!``
+- data lines
+- a noise parameters section, delimited by ``! NOISE PARAMETERS``
+
+Currently, only the first three sections are parsed.
+
+.. codeauthor::
+    Peter Kraus
+
 """
 
 import logging
@@ -48,31 +76,27 @@ logger = logging.getLogger(__name__)
 
 
 def process_filename(filename: str) -> dict:
-    if filename.endswith(".s1p"):
+    if filename.lower().endswith(".s1p"):
         params = ["_11"]
-    elif filename.endswith(".s2p"):
-        params = ["_11", "_12", "_21", "_22"]
+    elif filename.lower().endswith(".s2p"):
+        params = ["_11", "_21", "_12", "_22"]
     else:
         logger.warning("Could not determine number of ports, assuming 1 port.")
         params = ["_11"]
     return {"params": params}
 
 
-def process_option(line: str) -> dict:
+def process_options(line: str) -> dict:
     _, freq_unit, param, format, _, Rref = line.split()
-    if format == "DB":
+    if format == "DB" or format == "dB":
         columns = ["magnitude", "phase_angle"]
-
-        def conv(inp):
-            return 20 * np.log10(np.abs(inp))
+        mag_unit = "dB"
     elif format == "MA":
         columns = ["magnitude", "phase_angle"]
-
-        def conv(inp):
-            return inp
+        mag_unit = None
     elif format == "RI":
         columns = ["real", "imag"]
-        conv = None
+        mag_unit = None
     else:
         raise RuntimeError(f"Unsupported format: {format!r}")
 
@@ -80,19 +104,81 @@ def process_option(line: str) -> dict:
         "freq_unit": freq_unit.replace("HZ", "Hz"),
         "param": param,
         "columns": columns,
-        "conv": conv,
+        "mag_unit": mag_unit,
         "Rref": float(Rref),
     }
 
 
-def data_to_dataset(key: str, data: dict):
+def attr_in_lines(attr: str, lines: list[str], sep: str = ":") -> dict:
+    for line in lines:
+        if line.startswith(attr):
+            return {attr: line[line.index(sep) + 1 :].strip()}
+    return {}
+
+
+def process_comments(lines: list[str], tz: str) -> dict:
+    attrs = {}
+    uts = None
+    comstr = "\n".join(lines)
+
+    # No header - pass
+    if len(lines) == 0:
+        pass
+    # PicoVNA 108 files
+    elif "Ref Plane:" in comstr and len(lines) == 3:
+        fmt = "%m/%d/%Y  %I:%M:%S %p"
+        uts = dgutils.str_to_uts(
+            timestamp=lines[0], timezone=tz, format=fmt, strict=False
+        )
+        attrs["Ref Plane"] = lines[1].split(":")[1].strip()
+        attrs["Model"] = "PicoVNA 108"
+    # Agilent PNA (N523*) export
+    elif "Agilent Technologies" in lines[0] or "Keysight Technologies" in lines[0]:
+        _, model, serial, version = lines[0].split(",")
+        attrs["Model"] = model
+        attrs["Serial"] = serial
+        attrs["Software Version"] = version
+        attrs.update(attr_in_lines("Correction", lines, ":"))
+        datestr = attr_in_lines("Date", lines, ":")["Date"]
+        for fmt in ["%A, %B %d, %Y %H:%M:%S", "%a %b %d %H:%M:%S %Y"]:
+            uts = dgutils.str_to_uts(
+                timestamp=datestr, timezone=tz, format=fmt, strict=False
+            )
+            if uts is not None:
+                break
+    # Agilent N991* export
+    elif "Agilent N99" in lines[0] or "Keysight N99" in lines[0]:
+        attrs.update(attr_in_lines("Model", lines, ":"))
+        attrs.update(attr_in_lines("Serial", lines, ":"))
+        datestr = attr_in_lines("Date", lines, ":")["Date"]
+        fmt = "%A, %d %B %Y %H:%M:%S"
+        uts = dgutils.str_to_uts(
+            timestamp=datestr, timezone=tz, format=fmt, strict=False
+        )
+    # Rohde & Schwarz ZVA/ZVB export
+    elif "Rohde & Schwarz" in lines[0]:
+        model, version, serial = lines[0].split(" - ")
+        attrs["Model"] = model
+        attrs["Serial"] = serial
+        attrs["Software Version"] = version
+        datestr = attr_in_lines("Date", lines, ":")["Date"]
+        fmt = "%Y-%m-%d %H:%M:%S"
+        uts = dgutils.str_to_uts(
+            timestamp=datestr, timezone=tz, format=fmt, strict=False
+        )
+    # Unknown header - pass
+    else:
+        pass
+    return uts, attrs
+
+
+def data_to_dataset(key: str, data: dict) -> Dataset:
     coords = {"frequency": data["frequency"]}
     data_vars = {
         "frequency_std_err": DataArray(
             data=data["frequency_std_err"], dims=["frequency"]
         )
     }
-
     for k, v in data[key].items():
         data_vars[k] = DataArray(data=v, dims=coords.keys())
     return Dataset(data_vars=data_vars, coords=coords)
@@ -112,7 +198,7 @@ def extract(
     # Find options line
     for li in lines:
         if li.startswith("#"):
-            metadata.update(process_option(li))
+            metadata.update(process_options(li))
             # Only one options line is allowed - the rest is ignored, hence break
             break
 
@@ -126,7 +212,6 @@ def extract(
             data[k][m] = []
             data[k][f"{m}_std_err"] = []
             cols.append((k, m))
-    uts = None
     comments = []
 
     # Parse data lines
@@ -153,31 +238,16 @@ def extract(
                     data[k].append(val)
                     data[f"{k}_std_err"].append(dev)
                 elif k[1] == "magnitude":
-                    data[k[0]][k[1]].append(metadata["conv"](val))
-                    data[k[0]][f"{k[1]}_std_err"].append(metadata["conv"](dev))
+                    data[k[0]][k[1]].append(val)
+                    data[k[0]][f"{k[1]}_std_err"].append(dev)
                 else:
                     data[k[0]][k[1]].append(val)
                     data[k[0]][f"{k[1]}_std_err"].append(dev)
 
-    attrs = {}
-    for comment in comments:
-        for fmt in ["%m/%d/%Y  %I:%M:%S %p", None]:
-            # Try parsing it as timestamp
-            if uts is None:
-                uts = dgutils.str_to_uts(
-                    timestamp=comment, timezone=timezone, format=fmt, strict=False
-                )
-        else:
-            # Try to dig out metadata from the comment line
-            for sep in [":", "="]:
-                if sep in comment:
-                    parts = [part.strip() for part in comment.split(sep)]
-                    if len(parts) == 2:
-                        attrs[parts[0]] = parts[1]
-                        break
+    uts, attrs = process_comments(comments, timezone)
     attrs["Ref R"] = f"{metadata['Rref']} Ohm"
 
-    dtdict = {}
+    dtdict = {"/": Dataset(attrs=attrs)}
     for k in metadata["params"]:
         ds = data_to_dataset(key=k, data=data)
         for var in ds.variables:
@@ -191,11 +261,12 @@ def extract(
                 ds[var].attrs["units"] = "degree"
             elif "frequency" in var:
                 ds[var].attrs["units"] = metadata["freq_unit"]
+            elif "magnitude" in var and metadata["mag_unit"] is not None:
+                ds[var].attrs["units"] = metadata["mag_unit"]
         if uts is not None:
             ds = ds.expand_dims(dim=dict(uts=[uts]))
         else:
             ds.attrs["fulldate"] = False
         dtdict[f"/{k}"] = ds
     dt = DataTree.from_dict(dtdict)
-    dt.attrs = attrs
     return dt

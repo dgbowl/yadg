0b3a3580c7c7e60b1a91332afc1bf87b08fd6d36
Peter Kraus <peter.kraus@tu-berlin.de>
2024-05-22 20:23:51 +0200

    Remove duplicate eclab tests.

--- tests/test_eclab_mpt.py
+++ tests/test_eclab_mpt.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 import pickle
+import numpy as np
 from yadg.extractors.eclab.mpt import extract
 from .utils import compare_datatrees
 
@@ -41,3 +42,47 @@ def test_eclab_mpt(infile, datadir):
     with open(outfile, "wb") as out:
         pickle.dump(ret, out, 5)
     compare_datatrees(ret, ref)
+
+
+@pytest.mark.parametrize(
+    "afile, bfile",
+    [
+        ("mb_67.mpt", "mb_67.de.mpt"),
+    ],
+)
+def test_eclab_mpt_locale(afile, bfile, datadir):
+    os.chdir(datadir)
+    kwargs = dict(timezone="Europe/Berlin", encoding="windows-1252")
+    aret = extract(fn=afile, locale="en_US", **kwargs)
+    bret = extract(fn=bfile, locale="de_DE", **kwargs)
+    compare_datatrees(aret, bret)
+
+
+@pytest.mark.xfail(strict=False)
+@pytest.mark.parametrize(
+    "afile, bfile",
+    [
+        ("mb.old.mpt", "mb.mpt"),
+        ("gcpl.old.mpt", "gcpl.mpt"),
+        ("ocv.old.mpt", "ocv.mpt"),
+        ("wait.old.mpt", "wait.mpt"),
+    ],
+)
+def test_eclab_mpt_old(afile, bfile, datadir):
+    os.chdir(datadir)
+    kwargs = dict(timezone="Europe/Berlin", encoding="windows-1252")
+    aret = extract(fn=afile, locale="en_US", **kwargs)
+    bret = extract(fn=bfile, locale="en_US", **kwargs)
+    for key in aret.variables:
+        try:
+            if aret[key].dtype.kind in {"U"}:
+                np.testing.assert_array_equal(aret[key], bret[key])
+            else:
+                np.testing.assert_allclose(aret[key], bret[key], rtol=1e-3, atol=1e-11)
+            assert aret[key].attrs == bret[key].attrs
+        except AssertionError as e:
+            print(f"{key=}")
+            if key.endswith("_std_err"):
+                continue
+            else:
+                raise e

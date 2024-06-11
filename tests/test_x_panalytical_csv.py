5784b8dae674a99867104013f437c4e9e9247c22
Peter Kraus <peter.kraus@tu-berlin.de>
2024-06-11 21:53:23 +0200

    panalytical

---
+++ tests/test_x_panalytical_csv.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+import pickle
+from yadg.extractors.panalytical.csv import extract
+from .utils import compare_datatrees
+
+
+@pytest.mark.parametrize(
+    "infile",
+    [
+        "210520step1_30min.csv",
+    ],
+)
+def test_panalytical_csv(infile, datadir):
+    os.chdir(datadir)
+    ret = extract(fn=infile, encoding="utf-8", timezone="Europe/Berlin")
+    outfile = f"{infile}.pkl"
+    with open(outfile, "rb") as inp:
+        ref = pickle.load(inp)
+    print(f"{ret=}")
+    with open(outfile, "wb") as out:
+        pickle.dump(ret, out, 5)
+    compare_datatrees(ret, ref)
---
+++ tests/test_x_panalytical_xrdml.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+import pickle
+from yadg.extractors.panalytical.xrdml import extract
+from .utils import compare_datatrees
+
+
+@pytest.mark.parametrize(
+    "infile",
+    [
+        "210520step1_30min.xrdml",
+    ],
+)
+def test_panalytical_xrdml(infile, datadir):
+    os.chdir(datadir)
+    ret = extract(fn=infile, timezone="Europe/Berlin")
+    outfile = f"{infile}.pkl"
+    with open(outfile, "rb") as inp:
+        ref = pickle.load(inp)
+    print(f"{ret=}")
+    with open(outfile, "wb") as out:
+        pickle.dump(ret, out, 5)
+    compare_datatrees(ret, ref)

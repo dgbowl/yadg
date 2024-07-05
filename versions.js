5ba845c8094a2238357c6a4b67150865fcd33fdc
Peter Kraus <peter.kraus@tu-berlin.de>
2024-07-05 14:02:31 +0200

    master -> main

--- versions.js
+++ versions.js
@@ -3,7 +3,7 @@ page = arr[arr.length - 1];
 document.write('\
 <dl>\
     <dt>Versions</dt> \
-    <dd><a href="../master/' + page + '">master</a></dd>\
+    <dd><a href="../main/' + page + '">main</a></dd>\
     <dd><a href="../5.1/' + page + '">5.1</a></dd>\
     <dd><a href="../5.0/' + page + '">5.0</a></dd>\
     <dd><a href="../4.2/' + page + '">4.2</a></dd>\

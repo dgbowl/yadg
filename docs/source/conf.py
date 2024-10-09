d50e1f2e77f0aeeb1986f8e688858a63974873a5
Peter Kraus <peter.kraus@tu-berlin.de>
2024-10-09 07:47:01 +0200

    `eclab`: refactor `eclab.common` module (#187)
    * Remove eclab.common module
    
    * dgbowl schemas to main

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,7 @@ autodoc_pydantic_model_show_field_summary = False
 autodoc_pydantic_model_show_json = False
 autodoc_pydantic_model_member_order = "bysource"
 intersphinx_mapping = {
-    "dgbowl_schemas": ("https://dgbowl.github.io/dgbowl-schemas/master", None),
+    "dgbowl_schemas": ("https://dgbowl.github.io/dgbowl-schemas/main", None),
     "xarray": ("https://docs.xarray.dev/en/stable", None),
     "datatree": ("https://xarray-datatree.readthedocs.io/en/latest/", None),
     "babel": ("https://babel.pocoo.org/en/latest/", None),

32b30958ee2897e43490e8712c43d7cb96a36d84
Peter Kraus <peter.kraus@tu-berlin.de>
2024-10-10 07:08:46 +0200

    Specify locale in metadata tests.

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -23,7 +23,7 @@ from .utils import compare_datatrees
 def test_yadg_extractors_extract_with_metadata(filetype, infile, datadir):
     os.chdir(datadir)
     outfile = f"{infile}.nc"
-    ret = extract(filetype=filetype, path=infile)
+    ret = extract(filetype=filetype, path=infile, locale="en_GB")
     # ret.to_netcdf(f"C:/Users/Kraus/Code/yadg/tests/test_extract/{outfile}", engine="h5netcdf")
     ref = datatree.open_datatree(outfile, engine="h5netcdf")
     compare_datatrees(ret, ref, thislevel=True, descend=True)

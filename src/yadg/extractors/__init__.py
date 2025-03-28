b29f4bd69651e2482d4d3c58049e2ba93c01db01
carla-terboven <carla.terboven@helmholtz-berlin.de>
2025-03-28 15:02:13 +0100

    Rename extract parameters

--- src/yadg/extractors/__init__.py
+++ src/yadg/extractors/__init__.py
@@ -11,11 +11,12 @@ logger = logging.getLogger(__name__)
 
 def extract(
     filetype: str,
-    path: str,      # TODO maybe rename this to something like source?
+    source: str | bytes = None,
     timezone: str = None,
     encoding: str = None,
     locale: str = None,
     extract_func: str = "extract",
+    path: str = None,
     **kwargs: dict,
 ) -> DataTree:
     """
@@ -30,8 +31,8 @@ def extract(
     filetype:
         Specifies the filetype. Has to be a filetype supported by the dataschema.
 
-    path:
-        A :class:`pathlib.Path` object pointing to the file to be extracted.    # TODO
+    source:
+        Raw bytes of the file to be extracted, e.g. a file path or raw bytes of an mpr file.
 
     timezone:
         A :class:`str` containing the TZ identifier, e.g. "Europe/Berlin".
@@ -44,9 +45,26 @@ def extract(
 
     extract_func:
         A :class:`str` containing the extractor function to use, e.g. "extract_raw_content".
-        The default "extract" is using the file path to extract data.
+        Options:
+            - "extract" (default): Uses the file path to extract data.
+            - "extract_raw_content": Directly extracts from raw contents like bytes of an mpr file.
 
+    path:
+        Deprecated. Can now be specified using the source parameter.
+        A :class:`pathlib.Path` object pointing to the file to be extracted.
     """
+    if path is not None:
+        logger.info(
+            "The parameter 'path' is deprecated and has been replaced by 'source'. "
+            "Please use 'source' instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        source = path
+
+    if source is None:
+        raise ValueError("A valid 'source' must be provided.")
+
     extractor = ExtractorFactory(
         extractor={
             "filetype": filetype,
@@ -57,41 +75,6 @@ def extract(
     ).extractor
 
     return extract_from_source(path, extractor, extract_func)
-
-
-# TODO this function can probably be removed
-def extract_from_path(
-    path: str,
-    extractor: FileType,
-) -> DataTree:
-    """
-    Extracts data and metadata from the provided path using the supplied extractor.
-
-    The individual extractor functionality of yadg is called from here. The data is
-    always returned as a :class:`DataTree`. The ``original_metadata`` entries in
-    the returned objects are flattened using json serialisation. The returned objects
-    have a :func:`to_netcdf` as well as a :func:`to_dict` method, which can be used to
-    write the returned object into a file.
-    """
-
-    m = importlib.import_module(f"yadg.extractors.{extractor.filetype}")
-    func = getattr(m, "extract")
-
-    # Func should always return a xarray.DataTree
-    ret: DataTree = func(fn=path, **vars(extractor))
-    jsonize_orig_meta(ret)
-
-    ret.attrs.update(
-        {
-            "yadg_provenance": "yadg extract",
-            "yadg_extract_date": dgutils.now(asstr=True),
-            "yadg_extract_filename": str(path),
-            "yadg_extract_Extractor": extractor.model_dump_json(exclude_none=True),
-        }
-    )
-    ret.attrs.update(dgutils.get_yadg_metadata())
-
-    return ret
 
 
 def extract_from_source(
@@ -121,6 +104,7 @@ def extract_from_source(
         ret.attrs.update({"yadg_extract_filename": str(source),})
 
     return ret
+
 
 def jsonize_orig_meta(obj: DataTree):
     for k in obj:

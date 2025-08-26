11dee1de1a8a9dd522cf95105827a85f3f6ba7ea
Peter Kraus <peter.kraus@tu-berlin.de>
2025-08-26 07:18:52 +0200

    `eclab`: Implement support for BCD technique (#242)
    * assign typo
    
    * Implement BCD
    
    * tests
    
    * Docs.
    
    * fix docs
    
    * Set I/C 1 and Set I/C 2 in mpr & mpt
    
    * mpt fix

--- src/yadg/extractors/eclab/techniques.py
+++ src/yadg/extractors/eclab/techniques.py
@@ -20,6 +20,7 @@ Implemented techniques:
     - MP - Modular Potentio
     - CoV - Constant Voltage
     - CoC - Constant Current
+    - BCD - Battery Capacity Determination
 
 The module also implements resolution determination for parameters of techniques,
 in :func:`get_resolution`.
@@ -1318,6 +1319,52 @@ _coc_params_dtypes = [
     )
 ]
 
+# ~~~~~~~~~~~~~ Battery Capacity Determination ~~~~~~~~~~~~~
+_bcd_params_dtypes = [
+    (
+        np.dtype(
+            [
+                ("Set I/C 1", "|u1"),
+                ("Is1", "<f4"),
+                ("unit Is1", "|u1"),
+                ("N1", "<f4"),
+                ("I1 sign", "|u1"),
+                ("t1 (h:m:s)", "<f4"),
+                ("EM1 (V)", "<f4"),
+                ("EM1 vs.", "|u1"),
+                ("EM2 (V)", "<f4"),
+                ("EM2 vs.", "|u1"),
+                ("dE1 (mV)", "<f4"),
+                ("dt1 (s)", "<f4"),
+                ("Hold NONE/EM1/EM2", "|u1"),
+                ("tM (h:m:s)", "<f4"),
+                ("Set I limit", "|u1"),
+                ("Im", "<f4"),
+                ("unit Im", "|u1"),
+                ("Nl", "<f4"),
+                ("I Range", "|u1"),
+                ("Bandwidth", "|u1"),
+                ("E range min (V)", "<f4"),
+                ("E range max (V)", "<f4"),
+                ("tR (h:m:s)", "<f4"),
+                ("dER/dt (mV/h)", "<f4"),
+                ("dER (mV)", "<f4"),
+                ("dtR (s)", "<f4"),
+                ("Charge/Discharge", "|u1"),
+                ("Set I/C 2", "|u1"),
+                ("Is2", "<f4"),
+                ("unit Is2", "|u1"),
+                ("N2", "<f4"),
+                ("I2 sign", "|u1"),
+                ("t2 (h:m:s)", "<f4"),
+                ("Return Ei", "|u1"),
+                ("use C", "|u1"),
+            ]
+        ),
+        {"10.40"},
+    )
+]
+
 
 # Maps the technique byte to its corresponding dtype.
 technique_params_dtypes = {
@@ -1338,6 +1385,7 @@ technique_params_dtypes = {
     0x76: ("coC", _coc_params_dtypes),
     0x77: ("GCPL", _gcpl_params_dtypes),
     0x7F: ("MB", _mb_params_dtypes),
+    0x88: ("BCD", _bcd_params_dtypes),
 }
 
 unit_map = {
@@ -1369,6 +1417,12 @@ vs_map = (
     ("Emeas", 2),
     ("Ei", 3),
     ("Ref", 4),
+)
+
+ic_map = (
+    ("I", 0),
+    ("C / N", 1),
+    ("C x N", 2),
 )
 
 param_map = {
@@ -1582,6 +1636,8 @@ param_map = {
     "unit Imax": unit_map["I"],
     "unit Imin": unit_map["I"],
     "unit Is": unit_map["I"],
+    "unit Is1": unit_map["I"],
+    "unit Is2": unit_map["I"],
     "unit Is vs.": unit_map["vs."],
     "E (V) vs.": vs_map,
     "E1 (V) vs.": vs_map,
@@ -1590,12 +1646,11 @@ param_map = {
     "Ei (V) vs.": vs_map,
     "EL (V) vs.": vs_map,
     "Es (V) vs.": vs_map,
-    "Set I/C": (
-        ("I", 0),
-        ("C", 1),
-        ("C x N", 2),
-        ("C / N", 3),
-    ),
+    "EM1 vs.": vs_map,
+    "EM2 vs.": vs_map,
+    "Set I/C": ic_map,
+    "Set I/C 1": ic_map,
+    "Set I/C 2": ic_map,
     "Apply I/C": (
         ("I", 0),
         ("C", 1),
